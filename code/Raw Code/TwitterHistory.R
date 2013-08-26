71b81e43902387067076485d207a2c8b8a58a472
Paul K. Courtney <paul.k.courtney@icloud.com>
2013-08-26 16:02:32 -0400

    Twitter Feed

--- code/Raw Code/TwitterHistory.R
+++ code/Raw Code/TwitterHistory.R
@@ -5,3 +5,26 @@ require(knitr)
 require(plyr)
 require(reshape2)
 
+# load package twitteR
+library(twitteR)
+
+consumer_key <- "QWC5qPfawTebHMuelCQ"
+consumer_secret <- "R6KofepC9RIPzpbLdQhJl8OtbZxKa8lBCLzaLTbuTw"
+
+# Register the OAuth access tokens
+
+reqURL <- "https://api.twitter.com/oauth/request_token"
+accessURL <- "http://api.twitter.com/oauth/access_token"
+authURL <- "http://api.twitter.com/oauth/authorize"
+
+twitCred <- OAuthFactory$new(consumerKey=consumer_key,
+                             consumerSecret=consumer_secret,
+                             requestURL=reqURL,
+                             accessURL=accessURL,
+                             authURL=authURL)
+twitCred$handshake()
+registerTwitterOAuth(twitCred)
+
+
+# Search tweets with '#datamining'
+dmhash_tweets = searchTwitter("#datamining")
